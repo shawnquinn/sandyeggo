1cce555ad3f60d757445e97597f1b5399811a365
Shawn Quinn <shawnmq9@gmail.com>
2017-08-02 17:17:02 -0700

    Main Theme added

---
+++ front-page.php
@@ -0,0 +1,250 @@
+<?php
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+/**
+* Site Front Page
+******************
+* Globalize Theme Options
+*/
+global $lumos_options;
+$lumos_options = lumos_get_options();
+/**
+* If front page is set to display the
+* blog posts index, include home.php;
+* otherwise, display static front page
+* content
+*/
+if ( 'posts' == get_option( 'show_on_front' ) && $lumos_options['front_page'] != 1 ) {
+get_template_part( 'home' );
+} elseif ( 'page' == get_option( 'show_on_front' ) && $lumos_options['front_page'] != 1 ) {
+$template = get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true );
+$template = ( $template == 'default' ) ? 'index.php' : $template;
+locate_template( $template, true );
+} else {
+get_header();
+//test for first install no database
+$db = get_option( 'lumos_theme_options' );
+//test if all options are empty so we can display default text if they are
+$empty = ( empty( $lumos_options['home_headline'] ) && empty( $lumos_options['home_subheadline'] ) && empty( $lumos_options['home_content_area'] ) ) ? false : true;
+?>
+<section class="slider">
+  
+  <div class="boxed">
+	<div class="container hidden-md-up">
+		<div class="row align-items-center">
+	  	<div class="col-12">
+		  	<img class="mx-auto d-block img-fluid px-3" src="<?php echo get_template_directory_uri(); ?>/img/logo-sm.png" alt="Dr. Q Plastic Surgery" />
+	  	</div><!-- col -->
+	  </div><!-- row -->
+	</div><!-- .container -->
+    <div class="container-fluid hidden-md-down">
+      <div class="row align-items-center">
+        <div class="col-lg-2 offset-lg-1 columned">
+          <h3>Face</h3>
+          <nav class="procedure-menu">
+            <h4>Face</h4>
+            <?php wp_nav_menu(array( 'theme_location' => 'face-nav','menu_class' => 'featured-menu')); ?>
+          </nav>
+        </div>
+        <!-- col -->
+
+        <div class="col-lg-2 columned">
+          <h3>Breast</h3>
+          <nav class="procedure-menu">
+            <h4>Breast</h4>
+            <?php wp_nav_menu(array( 'theme_location' => 'breast-nav','menu_class' => 'featured-menu')); ?>
+          </nav>
+        </div>
+        <!-- col -->
+
+        <div class="col-lg-2 columned">
+          <h3>Body</h3>
+          <nav class="procedure-menu">
+            <h4>Body</h4>
+            <?php wp_nav_menu(array( 'theme_location' => 'body-nav','menu_class' => 'featured-menu')); ?>
+          </nav>
+        </div>
+        <!-- col -->
+
+        <div class="col-lg-2 columned">
+          <h3>Skin</h3>
+          <nav class="procedure-menu">
+            <h4>Skin</h4>
+            <?php wp_nav_menu(array( 'theme_location' => 'skin-nav','menu_class' => 'featured-menu')); ?>
+          </nav>
+        </div>
+        <!-- col -->
+
+        <div class="col-lg-2 columned">
+          <h3>Men</h3>
+          <nav class="procedure-menu">
+            <h4>Men</h4>
+            <?php wp_nav_menu(array( 'theme_location' => 'men-nav','menu_class' => 'featured-menu')); ?>
+          </nav>
+        </div>
+        <!-- col -->
+      </div>
+      <!-- row -->
+    </div>
+    <!-- container -->
+  </div>
+  <!-- boxed -->
+  <ul class="bx-slider">
+    <li id="one">
+    </li>
+    
+    <li id="two">
+    </li>
+    
+    <li id="three">
+    </li>
+  </ul>
+  <!-- bx-slider -->
+</section>
+<!--end of .slider -->
+<section class="home-content">
+  <div class="container">
+    <div id="primary">
+      <div id="content">
+        <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <?php get_template_part( 'loop-header' ); ?>
+        <?php lumos_entry_before(); ?>
+        <div id="post-<?php the_ID(); ?>"
+             <?php post_class(); ?> >
+        <?php lumos_entry_top(); ?>
+        <?php //get_template_part( 'post-meta-page' ); ?>
+        <div class="post-entry">
+          <?php the_content(__('Read more &#8250;', 'lumos')); ?>
+          <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'lumos'), 'after' => '</div>')); ?>
+        </div>
+        <!-- end of .post-entry -->
+        <?php get_template_part( 'post-data' ); ?>
+        <?php lumos_entry_bottom(); ?>
+      </div>
+      <!-- end of #post--->
+      <?php
+		endwhile;
+		get_template_part( 'loop-nav' );
+		else :
+		get_template_part( 'loop-no-posts' );
+		endif;
+		?>
+    </div>
+    <!-- end of #content -->
+  </div>
+  <!-- end of primary -->
+  </div>
+<!-- .container -->
+</section>
+<!-- section.home-content -->
+
+<section class="meet">
+	<div class="container">
+		
+		<div class="row align-items-center">
+			<div class="col-lg-5">
+				<img class="lime-border pb-5 img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/dr-q.jpg" alt="Dr. Q Plastic Surgery" />
+			</div><!-- col -->
+			
+			<div class="col-lg-6 offset-lg-1">
+				<h1 class="featured-title">Meet Dr Q <span>Suzanne M. Quardt, M.D.</span></h1>
+				<p>Dr Q ™ performs all aspects of Plastic Surgery including cosmetic surgery of the face and body, as well as challenging cases including reconstructions after breast, skin and other cancers, trauma, and other medical conditions.  Dr Q has a beautiful, state-of-the-art Cosmetic Surgery Center that also hosts her own Dr Q Medical Spa where she offers the latest and greatest in Non-Surgical technologies for procedures such as skin-tightening, fat-reduction, advanced skin-care including Hydrafacials, medical peels, laser resurfacing, sublative radiofrequency technology, microneedling and only the best, most effective and advanced Cosmeceutical skin-care lines.  Your treatments are performed in a luxuriously comfortable and serene, spa setting!  Her practice has expanded so rapidly that she is now happy to announce a new location with a private, accredited surgery center in Beverly Hills, California as well, to help serve her many patients that travel quite far to see her!</p>
+				<div class="row">
+				<div class="col-xl-6">
+				<button class="btn btn-lg btn-primary btn-block" href="<?php echo home_url('/meet-dr-quardt/'); ?>">Learn More</button>
+				</div>
+				</div>
+			</div><!-- col -->
+		</div><!-- row -->
+
+	</div><!-- .container -->
+</section><!-- section -->
+
+<section class="call-to-actions">
+	<div class="container">
+		<div class="row">
+			<div class="cta col-sm-4">
+				<a href="<?php echo home_url('/photo-gallery/'); ?>">
+					<div class="box">
+						<h4>Photo Gallery</h4>
+						<img class="w-100 d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/ctas/cta-1.jpg" alt="Photo Gallery" />
+					</div>
+				</a>
+			</div><!-- col -->
+			
+			<div class="cta col-sm-4">
+				<a href="<?php echo home_url('/specials/'); ?>">
+					<div class="box">
+						<h4>Current Specials</h4>
+						<img class="w-100 d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/ctas/cta-2.jpg" alt="Current Specials" />
+					</div>
+				</a>
+			</div><!-- col -->
+			
+			<div class="cta col-sm-4">
+				<a href="<?php echo home_url('/video-gallery/'); ?>">
+					<div class="box">
+						<h4>Video Gallery</h4>
+						<img class="w-100 d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/ctas/cta-3.jpg" alt="Video Gallery" />
+					</div>
+				</a>
+			</div><!-- col -->
+		</div><!-- row -->
+	</div><!-- .container -->
+</section><!-- section -->
+
+
+<section class="review">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-12">
+				<h2 class="featured-title">Testimonials <span>What Our Patients Are Saying</span></h2>
+
+				<div class="bx-review">
+					<div class="quote">
+						<div class="row align-items-center">
+							<div class="col-md-5">							
+								<blockquote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac enim scelerisque, congue nisi sit amet, posuere diam. Pellentesque fermentum purus in lorem lobortis laoreet. Donec maximus velit vel dapibus bibendum. Integer facilisis mauris facilisis ornare facilisis. Nunc ut ligula ut dolor porttitor consequat eget eu mi...” <span>Jane Doe, Rancho Mirage, CA</span></blockquote>
+							</div><!-- col -->
+							<div class="col-md-6 offset-md-1 hidden-sm-down">
+								<img class="lime-border pb-5 img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/reviews/r1.jpg" alt="Testimonial" />
+							</div><!-- col -->
+						</div><!-- row -->
+					</div><!-- quote -->
+					
+					<div class="quote">
+						<div class="row align-items-center">
+							<div class="col-md-5">							
+								<blockquote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac enim scelerisque, congue nisi sit amet, posuere diam. Pellentesque fermentum purus in lorem lobortis laoreet. Donec maximus velit vel dapibus bibendum. Integer facilisis mauris facilisis ornare facilisis. Nunc ut ligula ut dolor porttitor consequat eget eu mi...” <span>Joanna Smith, Palm Desert, CA</span></blockquote>
+							</div><!-- col -->
+							<div class="col-md-6 offset-md-1 hidden-sm-down">
+								<img class="lime-border pb-5 img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/reviews/r2.jpg" alt="Testimonial" />
+							</div><!-- col -->
+						</div><!-- row -->
+					</div><!-- quote -->
+					
+					<div class="quote">
+						<div class="row align-items-center">
+							<div class="col-md-5">							
+								<blockquote>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac enim scelerisque, congue nisi sit amet, posuere diam. Pellentesque fermentum purus in lorem lobortis laoreet. Donec maximus velit vel dapibus bibendum. Integer facilisis mauris facilisis ornare facilisis. Nunc ut ligula ut dolor porttitor consequat eget eu mi...” <span>Jamie Jones, Rancho Mirage, CA</span></blockquote>
+							</div><!-- col -->
+							<div class="col-md-6 offset-md-1 hidden-sm-down">
+								<img class="lime-border pb-5 img-fluid d-block mx-auto" src="<?php echo get_template_directory_uri(); ?>/img/reviews/r3.jpg" alt="Testimonial" />
+							</div><!-- col -->
+						</div><!-- row -->
+					</div><!-- quote -->
+					
+				</div><!-- bx-review -->
+
+				<div class="row pt-4">
+					<div class="col-md-5">
+						<button class="btn btn-lg btn-primary btn-block" href="<?php echo home_url(''); ?>">Learn More</button>
+					</div><!-- col -->
+				</div><!-- row -->
+			</div><!-- col -->
+		</div><!-- row -->
+	</div><!-- .container -->
+</section><!-- section -->
+
+<?php get_footer(); } ?>
---
+++ header.php
@@ -0,0 +1,166 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+/**
+* Header
+*/
+global $lumos_options;
+$lumos_options = lumos_get_options();
+?>
+<!DOCTYPE html>
+<html
+      <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <title>
+    <?php wp_title('|', true, 'right');?>
+  </title>
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <?php wp_head(); ?>
+</head>
+<body
+      <?php body_class(); ?> >
+
+<header id="header">
+  <div class="sticky-container sticky">
+		<div class="container-fluid">
+			<div class="row align-items-center">
+				<div class="col-lg-5 hidden-sm-down">
+					<div id="logo" >
+				      <h1>
+				        <a href="<?php echo home_url('/'); ?>">
+				          <img class="img-fluid d-block mx-auto" src="<?php header_image(); ?>" width="<?php if (function_exists('get_custom_header')) {
+                      echo get_custom_header() -> width;
+                  } else {
+                      echo HEADER_IMAGE_WIDTH;
+                  } ?>" height="<?php if (function_exists('get_custom_header')) {
+                      echo get_custom_header() -> height;
+                  } else {
+                      echo HEADER_IMAGE_HEIGHT;
+                  } ?>" alt="<?php bloginfo('name'); ?>" />
+				        </a>
+				      </h1>
+				    </div><!-- end of #logo -->
+				</div>
+				<div id="contact-bar" class="col-lg-7">
+					<div id="social" class="row align-items-center hidden-md-down">
+						<div class="col-xl-6 text-center text-xl-left">
+							<p>Schedule a Consultation: <a href="tel:+1-760-324-2660">(760) 324.2660</a></p>
+            </div>
+
+            <div class="col-xl-6 text-center text-xl-left">
+							<?php // First let's check if any of this was set
+
+                            echo '<ul class="social-icons my-3">';
+                            if (!empty($lumos_options['facebook_uid'])) {
+                                echo '<li><a target="_blank" class="icon-facebook" href="' . $lumos_options['facebook_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['twitter_uid'])) {
+                                echo '<li><a target="_blank" class="icon-twitter" href="' . $lumos_options['twitter_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['google_plus_uid'])) {
+                                echo '<li><a target="_blank" class="icon-google-plus" href="' . $lumos_options['google_plus_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['youtube_uid'])) {
+                                echo '<li><a target="_blank" class="icon-youtube" href="' . $lumos_options['youtube_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['linkedin_uid'])) {
+                                echo '<li><a target="_blank" class="icon-linkedin" href="' . $lumos_options['linkedin_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['yelp_uid'])) {
+                                echo '<li><a target="_blank" class="icon-yelp" href="' . $lumos_options['yelp_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['blogger_uid'])) {
+                                echo '<li><a target="_blank" class="icon-blogger" href="' . $lumos_options['blogger_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['instagram_uid'])) {
+                                echo '<li><a target="_blank" class="icon-instagram" href="' . $lumos_options['instagram_uid'] . '">'.'</a></li>';
+                            }
+                            if (!empty($lumos_options['foursquare_uid'])) {
+                                echo '<li><a target="_blank" class="icon-foursquare" href="' . $lumos_options['foursquare_uid'] . '">'.'</a></li>';
+                            }
+
+                            echo '</ul><!-- end of .social-icons -->';
+                            ?>
+						</div><!-- col -->
+					</div><!-- row -->
+					<div id="main-nav" class="row">
+						<div class="col-12 p-0">
+							<nav id="access">
+						      <ul class="mobile-nav hidden-lg-up">
+						        <li>
+						          <a href="<?php echo home_url('/contact/'); ?>">
+						            <i class="fa fa-envelope" aria-hidden="true">
+						            </i>
+						          </a>
+						        </li>
+						        <li>
+						          <a href="tel:+1-760-324-2660">
+						            <i class="fa fa-phone" aria-hidden="true">
+						            </i>
+						          </a>
+						        </li>
+						      </ul>
+						      <?php wp_nav_menu(array( 'theme_location' => 'header-nav','menu_class' => 'sf-menu menu-slick')); ?>
+						    </nav><!-- nav#access -->
+						</div><!-- col -->
+					</div><!-- row -->
+				</div><!-- col -->
+			</div><!-- row -->
+		</div><!-- container-fluid -->
+  </div>
+  <!-- .container-->
+  </div>
+  <!-- .container-->
+
+</header>
+<!-- #header -->
+
+
+<div id="main">
+  <div class="float-box" hidden>
+    <a class="clicker" href="<?php echo home_url(''); ?>">
+      <span>Contact Us
+      </span>
+    </a>
+    <div class="row">
+	    	<div class="col col-md-4">
+	      <address>
+	      </address>
+	      <a href="<?php echo home_url('/location/'); ?>">
+	        <img class="img-fluid mt-2" src="<?php echo get_template_directory_uri(); ?>/img/map.png" alt="Location" />
+	      </a>
+	    </div>
+	    <!-- end of col-1-3 -->
+	    <div class="col col-md-4">
+	      <?php echo do_shortcode('[contact-form-7 id="136" title="Contact form 1"]') ?>
+	    </div>
+	    <!-- end of col-1-3 -->
+	    <div class="col col-md-4 links d-flex">
+		  <div id="flex-item" class="w-100 d-block">
+		      <a href="<?php echo home_url(''); ?>">Current Specials
+		      </a>
+		      <a href="<?php echo home_url(''); ?>">Get Directions
+		      </a>
+		      <a href="<?php echo home_url('/photo-gallery/'); ?>">Photo Gallery
+		      </a>
+		  </div><!-- flex-item -->
+	    </div>
+	    <!-- end of col-1-3 -->
+    </div><!-- row -->
+  </div>
+  <!-- end of float-box -->
